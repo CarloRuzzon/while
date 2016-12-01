86e2585ae0e8f24aa9ad806068fec9e3692b87da
CarloRuzzon <carloruzzon@gmail.com>
2016-12-01 19:42:50 +0100

    Va bene?

--- litri.c
+++ litri.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 int main(){
-int km, cont=0;
+int km, cont=0, sentinella=-1;
 float litri=0, lkm, media, a=0;
 
-  while (litri!=-1){
+  while (litri!=sentinella){
     printf("Inserire il numero di litri per un pieno: ");
     scanf("%f", &litri);
   
 
-    if (litri!=-1){
+    if (litri!=sentinella){
       printf("Inserire il numero di km percorsi per un pieno: ");
       scanf("%d", &km); 
       lkm = km/litri;
       printf("Ha camminato %f km per litro\n", lkm);
       a=a+lkm;
       cont++;
-   
-  
-}
+   }
 }
 media = (float)a/cont;
 printf("La media totale e' %f\n", media);
