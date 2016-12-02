a5605d2ea142fecec47e1b2c5b34fafcb377c5c2
root <root@debian15>
2016-12-02 12:31:04 +0100

    Fatto

--- salari.c
+++ salari.c
@@ -4,7 +4,7 @@ int main(){
   int seninella=-1;
   printf("Inserire il prezzo totale degli articoli venduti (-1 per concludere): ");
   scanf("%f", &ven);
-  while (ven!=-1){
+  while (ven!=sentinella){
     ven=ven/100.*9.;
     sal=200.+ven;
     printf("Lo stipendio sara': %.2f\n", sal);
