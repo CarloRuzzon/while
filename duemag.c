0cd11f314b1890b82b64a7c733a0c1bc437a0d27
ALUNNO <alunno@debian15>
2016-12-12 08:51:21 +0100

    Fatto

--- duemag.c
+++ duemag.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-	int n, m1, m2=0, c=1;
-	printf("Inserire i numeri:");                    scanf("%d", &n);
-	m1=n;
-	while(c<10){
-		printf("Inserire i numeri:");                    scanf("%d", &n);
-		c++;
-		if (n>=m1 && n>=m2){
-			m2=m1;
-			m1=n;
-
-
-		}
-	}
-	printf("I numeri maggiori sono: %d e %d\n", m1, m2);
-
-	return 0;
-	}
-
+  int n, m1, m2, c=1;
+  printf("Inserire i numeri:");
+  scanf("%d", &n);
+  m1=n;
+  while(c<10){
+    printf("Inserire i  numeri:");
+    scanf("%d", &n);
+    c++;
+    if(n>=m1 && n>m2){
+      m2=m1;
+      m1=n;
+    }               
+    else if(n<m1 && n>m2){
+      m2=n;
+    }
+  }
+  printf("I numeri maggiori sono: %d e %d\n", m1, m2);
+  return 0;
+}
 
