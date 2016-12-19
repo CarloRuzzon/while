6397d6249278e21481c5cee2658b0afa772c652d
ALUNNO <alunno@debian15>
2016-12-19 08:34:49 +0100

    fatto

---
+++ dowhile.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+  int num, a, b=0;
+  do{
+    printf("Inserire un numero da 1 a 20:");
+    scanf("%d", &num);
+  }while(num<1 || num>20);
+    while(b<num){
+      a=0;
+    while(a<num){
+      printf("*");
+      a++;
+    }
+      b++;
+      printf("\n");
+    }
+    
+<<<<<<< HEAD
+=======
+  }while (num<1 || num>20);
+>>>>>>> 7a382a80991160deda258663975ab209b24e0d24
+  return 0;
+  }
